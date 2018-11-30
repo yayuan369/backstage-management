d2b624df1a6c6555cb8f0ad96c3ec7ff3f0a4e39
jinglijuan <lijuan.jing@meditrusthealth.com>
2018-11-30 14:55:47 +0800

    环境配置

--- src/main.js
+++ src/main.js
@@ -4,12 +4,28 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import Cookies from 'js-cookie';
+import store from './store';
+import i18n from './lang'; //导入语言
+import './premission'; //权限控制
+
+// 引入element-UI框架
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI, {
+  size: Cookies.get('size') || 'medium', //设置element-UI自适应尺寸
+  i18n: (key, value) => i18n.t(key, value) //兼容vue-i18n@6.x
+});
+
+
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  store,
+  i18n,
+  render: h => (App)
 })
