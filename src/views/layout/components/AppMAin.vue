be63d1428bd673a1fee8900621164adf7549a684
jinglijuan <lijuan.jing@meditrusthealth.com>
2018-12-04 18:17:26 +0800

    最新

---
+++ src/views/layout/components/AppMAin.vue
@@ -0,0 +1,32 @@
+<template>
+    <section class="app-main">
+        <transition name="fade-transform" mode="out-in">
+            <keep-alive :include="cacheViews">
+                <router-view :key="key" />
+            </keep-alive>
+        </transition>
+    </section>
+</template>
+
+<script>
+export default {
+  name: "AppMain",
+  computed: {
+    cacheViews() {
+      return this.$store.state.tagsView.cacheViews;
+    },
+    key() {
+      return this.$store.fullPath;
+    }
+  }
+};
+</script>
+<style lang='less' scoped>
+.app-main {
+  /*84 = navbar + tags-view = 50 +34 */
+  min-height: calc(100vh - 84px);
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+}
+</style>
