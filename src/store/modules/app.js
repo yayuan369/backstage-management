5f5414a56b20089d68a5a7fbefc45cd751c06e59
jinglijuan <lijuan.jing@meditrusthealth.com>
2018-12-04 10:43:59 +0800

    暂存

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -1,7 +1,54 @@
-
+import Cookies from 'js-cookie'
 
 const app = {
-
+  state: {
+    sidebar: {
+      opened: !+Cookies.get('sidebarStatus'),
+      withoutAnimation: false
+    },
+    device: 'desktop'
+  },
+  //   mutation 必须是同步函数
+  mutations: {
+    TOGGLE_SIDEBAR: state => {
+      if (state.sidebar.opened) {
+        Cookies.set('sidebarStatus', 1);
+      } else {
+        Cookies.set('sidebarStatus', 0);
+      }
+      state.sidebar.opened = !state.sidebar.opened;
+      state.sidebar.withoutAnimation = false;
+    },
+    CLOSE_SIDEBAR: (state, withoutAnimation) => {
+      Cookies.set('sidebarStatus', 1);
+      state.sidebar.opened = false;
+      state.sidebar.withoutAnimation = withoutAnimation;
+    },
+    TOGGLE_DEVICE: (state, device) => {
+      state.device = device;
+    }
+  },
+  // action 提交的是 mutation，而不是直接变更状态。
+  // action 可以包含任意异步操作
+  actions: {
+    ToggleSideBar: ({
+      commit
+    }) => {
+      commit('TOGGLE_SIDEBAR');
+    },
+    CloseSideBar({
+      commit
+    }, {
+      withoutAnimation
+    }) {
+      commit('CLOSE_SIDEBAR', withoutAnimation)
+    },
+    ToggleDevice({
+      commit
+    }, device) {
+      commit('TOGGLE_DEVICE', device)
+    }
+  }
 }
 
 export default app
