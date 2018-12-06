0de1e923b15b8d3a46885fea045a808c5f74c703
jinglijuan <lijuan.jing@meditrusthealth.com>
2018-12-06 16:37:32 +0800

    主页补充

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="login-container">
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
-      <h3 class="title">backstage-management</h3>
+       <div class="title-container">
+        <h3 class="title">{{ $t('login.title') }}</h3>
+        <lang-select class="set-language"/>
+      </div>
       <el-form-item prop="username">
         <span class="svg-container">
           <svg-icon icon-class="#icon-gerenxinxi" />
         </span>
-        <el-input v-model="loginForm.username" name="username" type="text" auto-complete="on" placeholder="username" />
+        <el-input v-model="loginForm.username" name="username" type="text" auto-complete="on" :placeholder="$t('login.username')" />
       </el-form-item>
       <el-form-item prop="password">
         <span class="svg-container">
@@ -17,7 +20,7 @@
           v-model="loginForm.password"
           name="password"
           auto-complete="on"
-          placeholder="password"
+          :placeholder="$t('login.password')"
           @keyup.enter.native="handleLogin" />
         <span class="show-pwd" @click="showPwd">
           <svg-icon icon-class="#icon-biyan" />
@@ -25,34 +28,59 @@
       </el-form-item>
       <el-form-item>
         <el-button :loading="loading" type="primary" style="width:100%;" @click.native.prevent="handleLogin">
-          Sign in
+          {{ $t('login.logIn') }}
         </el-button>
       </el-form-item>
       <div class="tips">
         <span style="margin-right:20px;">username: admin</span>
         <span> password: admin</span>
       </div>
+
+
+      <div class="tips">
+        <span>{{ $t('login.username') }} : admin</span>
+        <span>{{ $t('login.password') }} : {{ $t('login.any') }}</span>
+      </div>
+      <div class="tips">
+        <span style="margin-right:18px;">{{ $t('login.username') }} : editor</span>
+        <span>{{ $t('login.password') }} : {{ $t('login.any') }}</span>
+      </div>
+
+      <el-button class="thirdparty-button" type="primary" @click="showDialog=true">{{ $t('login.thirdparty') }}</el-button>
+
     </el-form>
+
+    <el-dialog :title="$t('login.thirdparty')" :visible.sync="showDialog" append-to-body>
+      {{ $t('login.thirdpartyTips') }}
+      <br>
+      <br>
+      <br>
+      <social-sign />
+    </el-dialog>
+
   </div>
 </template>
 
 <script>
 import { isvalidUsername } from '@/utils/validate'
+import LangSelect from '@/components/LangSelect'
+import SocialSign from './socialsignin'
 
 
 export default {
   name: 'Login',
+  components: { LangSelect, SocialSign },
   data() {
     const validateUsername = (rule, value, callback) => {
       if (!isvalidUsername(value)) {
-        callback(new Error('请输入正确的用户名'))
+        callback(new Error('Please enter the correct user name'))
       } else {
         callback()
       }
     }
     const validatePass = (rule, value, callback) => {
-      if (value.length < 5) {
-        callback(new Error('密码不能小于5位'))
+      if (value.length < 6) {
+        callback(new Error('The password can not be less than 6 digits'))
       } else {
         callback()
       }
@@ -67,6 +95,7 @@ export default {
         password: [{ required: true, trigger: 'blur', validator: validatePass }]
       },
       loading: false,
+      showDialog: false,
       pwdType: 'password',
       redirect: undefined
     }
@@ -109,41 +138,62 @@ export default {
 </script>
 
 <style lang="less">
-.login-container {
-  .el-input {
-    display: inline-block;
-    height: 47px;
-    width: 85%;
-    input {
-      background: transparent;
-      border: 0px;
-      -webkit-appearance: none;
-      border-radius: 0px;
-      padding: 12px 5px 12px 15px;
-      color: #eee;
-      height: 47px;
-      &:-webkit-autofill {
-        -webkit-box-shadow: 0 0 0px 1000px #2d3a4b inset !important;
-        -webkit-text-fill-color: #fff !important;
+  /* 修复input 背景不协调 和光标变色 */
+  /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
+
+  @bg:#283443;
+  @light_gray:#eee;
+  @cursor: #fff;
+
+  @supports (-webkit-mask: none) and (not (cater-color: @cursor)) {
+    .login-container .el-input input{
+      color: @cursor;
+      &::first-line {
+        color: @light_gray;
       }
     }
   }
-  .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    color: #454545;
-  }
-}
 
+  /* reset element-ui css */
+  .login-container {
+    .el-input {
+      display: inline-block;
+      height: 47px;
+      width: 85%;
+      input {
+        background: transparent;
+        border: 0px;
+        -webkit-appearance: none;
+        border-radius: 0px;
+        padding: 12px 5px 12px 15px;
+        color: @light_gray;
+        height: 47px;
+        caret-color: @cursor;
+        &:-webkit-autofill {
+          -webkit-box-shadow: 0 0 0px 1000px @bg inset !important;
+          -webkit-text-fill-color: @cursor !important;
+        }
+      }
+    }
+    .el-form-item {
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      background: rgba(0, 0, 0, 0.1);
+      border-radius: 5px;
+      color: #454545;
+    }
+  }
 </style>
 
 <style lang="less" scoped>
+@bg:#2d3a4b;
+@dark_gray:#889aa4;
+@light_gray:#eee;
+
 .login-container {
   position: fixed;
   height: 100%;
   width: 100%;
-  background-color: #2d3a4b;
+  background-color: @bg;
   .login-form {
     position: absolute;
     left: 0;
@@ -165,27 +215,41 @@ export default {
   }
   .svg-container {
     padding: 6px 5px 6px 15px;
-    color: #889aa4;
+    color: @dark_gray;
     vertical-align: middle;
     width: 30px;
     display: inline-block;
   }
-  .title {
-    font-size: 26px;
-    font-weight: 400;
-    color: #eee;
-    margin: 0px auto 40px auto;
-    text-align: center;
-    font-weight: bold;
+  .title-container {
+    position: relative;
+    .title {
+      font-size: 26px;
+      color: @light_gray;
+      margin: 0px auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+    .set-language {
+      color: #fff;
+      position: absolute;
+      top: 5px;
+      right: 0px;
+    }
   }
   .show-pwd {
     position: absolute;
     right: 10px;
     top: 7px;
     font-size: 16px;
-    color: #889aa4;
+    color: @dark_gray;
     cursor: pointer;
     user-select: none;
   }
+  .thirdparty-button {
+    position: absolute;
+    right: 35px;
+    bottom: 28px;
+  }
 }
 </style>
+
